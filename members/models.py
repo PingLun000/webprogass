f7128110b8deb5634f985b3353b721a89e600bf1
pinglun000 <pingluntan1110@gmail.com>
2021-12-09 17:28:52 +0800

    commit

--- members/models.py
+++ members/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-'''class members(models.Model): 
+class members(models.Model): 
     
+
     email=models.CharField(max_length=255,unique=True)
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
-    
     contact_number=models.CharField(max_length=255,unique=True)
     image=models.ImageField(upload_to='image/products')
- 
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     
      
      
     def __str__(self):
-        return self.email '''
+        return self.email 
